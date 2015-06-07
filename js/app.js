3b67de31ef8edcb8972a027f4ef80e3b24a9d1a8
Zack Klinger <zklinger@gmail.com>
2015-06-06 17:55:56 -0700

    ADD logic to Insert a new document (record) into the restaurants DB

--- js/app.js
+++ js/app.js
@@ -1,10 +1,45 @@
+//requires
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-
+var ObjectId = require('mongodb').ObjectID;
+//location of the mongoDB named 'test'
 var url = 'mongodb://localhost:27017/test';
+
+//Insert a document into a collection named restaurants.
+//The operation will create the collection if it does not already exist.
+var insertDocument = function(db, callback) {
+	db.collection('restaurants').insertOne( {
+		"address" : {
+			"street" : "2 Avenue",
+			"zipcode" : "10075",
+			"building" : "1480",
+			"coord" : [-73.9557413, 40.7720266],
+		},
+		"borough" : "Manhattan",
+		"cuisine" : "Italian",
+		"grades" : [
+			{
+				"date" : new Date("2014-10-01T00:00:00Z"),
+				"grade" : "A",
+				"score" : 11
+			},
+			{
+				"date" : new Date("2014-01-16T00:00:00Z"),
+				"grade" : "B",
+				"score" : 17
+			}
+		],
+		"name" : "Vella",
+		"restaurant_id" : "41704620"
+	}, function(err, result) {
+		assert.equal(err, null);
+		console.log("Inserted a document into the restaurants collection.");
+	});
+};
 
 MongoClient.connect(url, function(err, db) {
 	assert.equal(null, err);
-	console.log("Connected correctly to server.");
-	db.close();
+	insertDocument(db, function() {
+		db.close();
+	});
 });
