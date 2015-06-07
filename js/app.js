9b72d898e35741e8692ba5bcb4b15d220fb689d4
Zack Klinger <zklinger@gmail.com>
2015-06-07 12:42:57 -0700

    CHANGE logic to query all documents in the database

--- js/app.js
+++ js/app.js
@@ -5,41 +5,23 @@ var ObjectId = require('mongodb').ObjectID;
 //location of the mongoDB named 'test'
 var url = 'mongodb://localhost:27017/test';
 
-//Insert a document into a collection named restaurants.
-//The operation will create the collection if it does not already exist.
-var insertDocument = function(db, callback) {
-	db.collection('restaurants').insertOne( {
-		"address" : {
-			"street" : "2 Avenue",
-			"zipcode" : "10075",
-			"building" : "1480",
-			"coord" : [-73.9557413, 40.7720266],
-		},
-		"borough" : "Manhattan",
-		"cuisine" : "Italian",
-		"grades" : [
-			{
-				"date" : new Date("2014-10-01T00:00:00Z"),
-				"grade" : "A",
-				"score" : 11
-			},
-			{
-				"date" : new Date("2014-01-16T00:00:00Z"),
-				"grade" : "B",
-				"score" : 17
-			}
-		],
-		"name" : "Vella",
-		"restaurant_id" : "41704620"
-	}, function(err, result) {
+//To return all documents in a collection, call the find method without
+//a criteria document.
+var findRestaurants = function(db, callback) {
+	var cursor = db.collection('restaurants').find( );
+	cursor.each(function(err, doc) {
 		assert.equal(err, null);
-		console.log("Inserted a document into the restaurants collection.");
+		if (doc != null) {
+			console.dir(doc);
+		} else {
+			callback();
+		}
 	});
 };
 
 MongoClient.connect(url, function(err, db) {
 	assert.equal(null, err);
-	insertDocument(db, function() {
+	findRestaurants(db, function() {
 		db.close();
 	});
 });
